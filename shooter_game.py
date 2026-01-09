e944b001b45cf4b8588a2265654bc85e16226aac
harveyzhenghw <harvey.zheng.hw@gmail.com>
2026-01-09 15:21:26 +0800

    Initialize Pygame window for shooter game
    Added basic Pygame setup with screen initialization and window caption. Defined screen width and height, and created the display surface for the game.

---
+++ shooter_game.py
@@ -0,0 +1,9 @@
+#import and int676
+import pygame
+pygame.init()
+#screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 0.8 * SCREEN_WIDTH
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brave Hero")
