54252356f6542e444e434a6d7359ebc4718eb612
harveyzhenghw <harvey.zheng.hw@gmail.com>
2026-01-09 15:29:39 +0800

    Add game loop and settings for shooter game
    Introduced a basic game loop, event handling, and screen update logic to shooter_game.py. Added .vscode/settings.json to configure Python REPL behavior.

--- shooter_game.py
+++ shooter_game.py
@@ -7,3 +7,27 @@ SCREEN_HEIGHT = 0.8 * SCREEN_WIDTH
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brave Hero")
+#clock
+clock = pygame.time.Clock()
+FPS = 60
+
+#variables
+game_is_running = True
+background_color = (0,0,0)
+
+#game loop
+while game_is_running:
+    clock.tick(FPS)
+    #fill background
+    screen.fill(background_color)
+
+    #game events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_is_running = False
+
+    #update display
+    pygame.display.update()
+
+#quit pygame
+pygame.quit()
